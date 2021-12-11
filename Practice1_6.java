ef053326b848de4a40404e4c5e1bb2a64ee5dd75
otinu <fitshter@gmail.com>
2021-12-11 17:14:50 +0900

    [add]1-6

---
+++ Practice1_6.java
@@ -0,0 +1,11 @@
+package workbook;
+
+public class Practice1_6 {
+	public static void main(String[] args) {
+		int x = 7;
+		x *= 3;
+		System.out.println(x);
+		x /= 2;
+		System.out.println(x);
+	}
+}
