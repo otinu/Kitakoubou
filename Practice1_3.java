ecd495b8cde96f2bb9c91a3d4fab5850f96ccdd3
otinu <fitshter@gmail.com>
2021-12-11 17:06:19 +0900

    [add]1-3

---
+++ Practice1_3.java
@@ -0,0 +1,11 @@
+package workbook;
+
+public class Practice1_3 {
+
+	public static void main(String[] args) {
+		Integer x = 13;
+		Integer y = 17;
+		System.out.println("x=" + x.toString() + ",y=" + y.toString());
+	}
+
+}
