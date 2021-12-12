5a19d31d8fcb50cab65bfd277d9099e4736cb3b6
otinu <fitshter@gmail.com>
2021-12-12 09:25:53 +0900

    [add]Practice1-12

---
+++ Practice1_12.java
@@ -0,0 +1,16 @@
+package workbook;
+
+public class Practice1_12 {
+	public static void main(String[] args) {
+		
+		System.out.println("インクリメントの表示");
+		for(int i = 0; i < 5; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("デクリメントの表示");
+		for(int i = 4; i >= 0; i--) {
+			System.out.println(i);
+		}
+	}
+}
