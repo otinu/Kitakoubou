261a0c7ceea0d2c6c5838249fe98345805515286
otinu <fitshter@gmail.com>
2021-12-12 09:22:19 +0900

    [add]Practice1-11

---
+++ Practice1_11.java
@@ -0,0 +1,10 @@
+package workbook;
+
+public class Practice1_11 {
+	public static void main(String[] args) {
+		int x = 10;
+		int divideNum = 2;
+		System.out.println(x / divideNum);
+		System.out.println(x % divideNum);
+	}
+}
