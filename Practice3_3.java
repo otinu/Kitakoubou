71f2e1acfbe74966a639af9fe9d468ea6838f9ef
otinu <fitshter@gmail.com>
2021-12-14 05:19:43 +0900

    [add]Practice3-3

---
+++ Practice3_3.java
@@ -0,0 +1,22 @@
+package workbook;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practice3_3 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(
+                                new InputStreamReader( System.in ) );
+
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        if(x > y) {
+        	System.out.println("xはyより大きい");
+        } else {
+        	System.out.println("xはyより小さい");
+        }
+	}
+
+}
