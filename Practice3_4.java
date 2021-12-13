41d9f56830be4c116ce341c7be89817a6ac2d75c
otinu <fitshter@gmail.com>
2021-12-14 05:25:32 +0900

    [fix]Practice3-4

--- Practice3_4.java
+++ Practice3_4.java
@@ -1,10 +1,24 @@
 package workbook;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Practice3_4 {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(
+                                new InputStreamReader( System.in ) );
 
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        if(x > y) {
+        	System.out.println("xはyより大きい");
+        } else if(x < y) {
+        	System.out.println("xはyより小さい");
+        } else {
+        	System.out.println("xとyは等しい");
+        }
 	}
 
 }
