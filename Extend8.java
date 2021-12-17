cc75e3f86410ef3895c76e789e8d2b0b534b3819
otinu <fitshter@gmail.com>
2021-12-17 16:57:00 +0900

    [add]comment

--- Extend8.java
+++ Extend8.java
@@ -11,7 +11,11 @@ class B2 extends A2 {
 public class Extend8 {
 
 	public static void main(String[] args) {
+		
+		//B型(サブクラス)のインスタンスを作成して、A型(スーパークラス)で扱うことを指定
 		A2 a = new B2();
+		
+		//A型として扱われていたB型のインスタンスを本来のB型として扱うよう、キャスト
 		B2 b = (B2) a;
 		b.hello();
 	}
