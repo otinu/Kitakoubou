d3578e030ad2ca60857755089e81fc5bf2f13df2
otinu <fitshter@gmail.com>
2021-12-18 16:22:19 +0900

    [add]Extend11

---
+++ Extend11.java
@@ -0,0 +1,21 @@
+package workbook;
+
+import java.util.ArrayList;
+
+public class Extend11 {
+
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<>();
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		
+		// 要素番号2番の要素が「X」で上書きされるのではなく、
+		// 要素番号2番に「X」が入り、要素番号3番に「C」が移動する
+		list.add(2, "X");
+		for(String str : list) {
+			System.out.println(str);
+		}
+	}
+
+}
