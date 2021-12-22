bc272c0c10df25e026ad89c5eed2ab1ca241e270
otinu <fitshter@gmail.com>
2021-12-22 09:54:20 +0900

    [add]Extend15

---
+++ Extend15.java
@@ -0,0 +1,12 @@
+package workbook;
+
+public class Extend15 {
+
+	public static void main(String[] args) {
+		String name = "kiss";
+		
+		// substringメソッドでは、「(第一引数)番目～(第二引数の1個前)番目まで」の文字列を引き抜く
+		System.out.println(name.substring(1, 4));
+	}
+
+}
