8642c617bd793c60383ecf5875b53e0f58cc756d
otinu <fitshter@gmail.com>
2021-12-23 17:50:04 +0900

    [add]Practice4-9

---
+++ Practice4_9.java
@@ -0,0 +1,23 @@
+package workbook;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class Practice4_9 {
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < 10; i++) {
+			Random random = new Random();
+			int randomValue = random.nextInt(101) - 50;
+			list.add(randomValue);
+			System.out.print(randomValue + ", ");
+			if(i == 9) {
+				System.out.print("\n");
+			}
+		}
+		System.out.println("配列の最大値: " + Collections.max(list));
+		System.out.println("配列の最小値: " + Collections.min(list));
+	}
+}
