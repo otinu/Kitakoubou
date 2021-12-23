2d3ca89768587fdcb40b14307d00b0a665cb3e22
otinu <fitshter@gmail.com>
2021-12-23 17:01:40 +0900

    [add]Practice4-5

---
+++ Practice4_5.java
@@ -0,0 +1,13 @@
+package workbook;
+
+public class Practice4_5 {
+
+	public static void main(String[] args) {
+		int total = 1;
+		for (int i = 7; 0 < i; i--) {
+			total *= i;
+		}
+		System.out.println(total);
+	}
+
+}
