2e2e50810261281bca8dcaa9eebda414034d98dc
otinu <fitshter@gmail.com>
2021-12-24 16:22:28 +0900

    [add]Extend21

---
+++ Extend21.java
@@ -0,0 +1,15 @@
+package workbook;
+
+public class Extend21 {
+
+	public static void main(String[] args) {
+		
+		// 浮動小数点のデフォルトはdoubleとして扱われる
+		// float型の変数を初期化する場合はリテラルをfloat型にキャストして代入する
+		
+		double a = 10.0;
+		// float b = 10.0;
+		float b = (float) 10.0;
+	}
+
+}
