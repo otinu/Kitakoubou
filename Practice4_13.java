3db5c37c929413764923fe32d9e3517029b41251
otinu <fitshter@gmail.com>
2021-12-27 12:53:33 +0900

    [add]Practice4-13

---
+++ Practice4_13.java
@@ -0,0 +1,28 @@
+package workbook;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practice4_13 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		{
+			System.out.println("1: ストライク　2: ボール");
+	        int strikeCount = 0;
+	        int ballCount = 0;
+	        
+	        int x;
+	        while (strikeCount < 4 || ballCount < 5) {
+	        	BufferedReader br = new BufferedReader(
+                        new InputStreamReader( System.in ) );
+	        	x = Integer.parseInt(br.readLine());
+	        	if (x == 1) {
+	        		strikeCount++;
+		        } else if(x == 2) {
+		        	ballCount++;
+		        }
+	        }
+	        System.out.println(ballCount + "ボール、" + strikeCount + "ストライク");
+	    }
+	}
+}
