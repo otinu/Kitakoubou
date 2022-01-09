5bfda5610d811a0f2171d1d6164d207f93f884fb
otinu <fitshter@gmail.com>
2022-01-09 15:37:57 +0900

    [add]Extend30

---
+++ Extend30.java
@@ -0,0 +1,14 @@
+package workbook;
+
+final class dummy {
+	
+}
+
+// mainメソッドの前に別のクラスを定義してしまうと、javaコマンドでファイルを実行することができない
+public class Extend30 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
