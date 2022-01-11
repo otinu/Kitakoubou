b4d93011d38cff9222fb973ea92b41d96e987d3f
otinu <fitshter@gmail.com>
2022-01-11 21:35:57 +0900

    [add]Extend36

---
+++ Extend36.java
@@ -0,0 +1,18 @@
+package workbook;
+
+public class Extend36 {
+
+	public static void main(String[] args) {
+		
+		// 8進数の記述
+		// ⇒「7」まで
+		int x = 077;
+		int y = 078;
+		
+		// 16進数の記述
+		// ⇒ 「f」まで
+		int a = 0x4ac;
+		int b = 0x8fg;
+	}
+
+}
