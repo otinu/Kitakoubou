a05ed1f5100d87a8e38170e6cf0c1441ce82a500
otinu <fitshter@gmail.com>
2022-01-15 07:45:04 +0900

    [add]Extend-37

---
+++ Extend37.java
@@ -0,0 +1,29 @@
+package workbook;
+
+public class Extend37 {
+	
+	/*
+	int static private  test() {
+		int num = 0;
+		return num;
+	}
+	*/
+
+	/*
+	 static int private  test() {
+		int num = 0;
+		return num;
+	}
+	*/
+	
+	int private static  test() {
+		int num = 0;
+		return num;
+	}
+	
+	private int static  test() {
+		int num = 0;
+		return num;
+	}
+
+}
