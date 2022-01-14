1504fd7c7e65d04e1924bc45e065e7ac480a1b05
otinu <fitshter@gmail.com>
2022-01-15 07:58:07 +0900

    [add]Extend38

--- Extend37.java
+++ Extend37.java
@@ -2,19 +2,18 @@ package workbook;
 
 public class Extend37 {
 	
+	// 戻り値の型の指定はメソッド名の直前に記述しないとコンパイルエラーとなる
+	
 	/*
 	int static private  test() {
 		int num = 0;
 		return num;
 	}
-	*/
 
-	/*
 	 static int private  test() {
 		int num = 0;
 		return num;
 	}
-	*/
 	
 	int private static  test() {
 		int num = 0;
@@ -22,6 +21,12 @@ public class Extend37 {
 	}
 	
 	private int static  test() {
+		int num = 0;
+		return num;
+	}
+	*/
+	
+	static private int test() {
 		int num = 0;
 		return num;
 	}
---
+++ Extend38.java
@@ -0,0 +1,9 @@
+package workbook;
+
+public class Extend38 {
+
+	public static void main(String[] args) {
+		System.out.println(args[0] + args[1]);
+	}
+
+}
