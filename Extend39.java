63e0c1ccc9dc01a7af3910d07bfce9cd74ad2ca0
otinu <fitshter@gmail.com>
2022-01-15 08:37:29 +0900

    [add]Extend39

---
+++ Extend39.java
@@ -0,0 +1,11 @@
+package workbook;
+
+public class Extend39 {
+
+	// var型では、複数の変数を一度に初期化することはできない
+	public static void main(String[] args) {
+		int i1, i2, i3 = 10;
+		var v1, v2, v3 = "Setting";
+	}
+
+}
