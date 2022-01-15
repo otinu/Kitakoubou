4e174a72934d55f9507e0e9735173449bb226a7d
otinu <fitshter@gmail.com>
2022-01-15 19:04:46 +0900

    [add]Extend43

---
+++ Extend43.java
@@ -0,0 +1,20 @@
+package workbook;
+
+public class Extend43 {
+
+	public static void main(String[] args) {
+		
+		// 8 * 2 + 2 = 18
+		// 18 * 3 + 1 = 55
+		System.out.println(Integer.parseInt("31", 022));
+		
+		// 16 * 1 + 1 = 17
+		// 17 * 1 + 1 = 18
+		System.out.println(Integer.parseInt("11", 0x11));
+		
+		// 8 + 2 + 1 = 11
+		// 11 * 1 + 0 = 11
+		System.out.println(Integer.parseInt("10", 0b1011));
+	}
+
+}
