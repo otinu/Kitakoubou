dfe2d89e60616bd199f57a00e6669b7e243c6673
otinu <fitshter@gmail.com>
2022-01-15 21:20:24 +0900

    [fix]Extend43

--- Extend43.java
+++ Extend43.java
@@ -4,16 +4,30 @@ public class Extend43 {
 
 	public static void main(String[] args) {
 		
-		// 8 * 2 + 2 = 18
-		// 18 * 3 + 1 = 55
+		// 8 + 1*6 = 14
+		System.out.println(Integer.parseInt("16", 8));
+		
+		// 11*2 + 1*1  = 23
+		System.out.println(Integer.parseInt("21", 11));
+		
+		// 11*2 + 1*1  = 23
+		System.out.println(Integer.parseInt("21", 11));
+		
+		// 16*3 + 1*2  = 50
+		System.out.println(Integer.parseInt("32", 16));
+		
+		//----------------------
+		
+		// 8*2 + 2 = 18
+		// 18*3 + 1 = 55
 		System.out.println(Integer.parseInt("31", 022));
 		
-		// 16 * 1 + 1 = 17
-		// 17 * 1 + 1 = 18
+		// 16*1 + 1 = 17
+		// 17*1 + 1 = 18
 		System.out.println(Integer.parseInt("11", 0x11));
 		
 		// 8 + 2 + 1 = 11
-		// 11 * 1 + 0 = 11
+		// 11*1 + 1*0 = 11
 		System.out.println(Integer.parseInt("10", 0b1011));
 	}
 
