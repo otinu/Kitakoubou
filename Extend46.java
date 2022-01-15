a7472fb5c4fd9a6414bcc48e38fe2f6ba69cc414
otinu <fitshter@gmail.com>
2022-01-15 20:27:10 +0900

    [add]Extend46

---
+++ Extend46.java
@@ -0,0 +1,14 @@
+package workbook;
+
+public class Extend46 {
+
+	public static void main(String[] args) {
+		
+		// 初期化子を使ってvar型の変数に入れる場合、newを使わないと初期化できない
+		// ⇒右辺で何の配列型であるか明示しないと、varが型推論できない
+		int[] array = { 1, 2, 3 };
+		var list = { 1, 2, 3 };
+		var list2 = new int[] { 1, 2, 3 };
+	}
+
+}
