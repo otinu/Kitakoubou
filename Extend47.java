7b46960629fd209558bfd81e24c2e606b2dd026e
otinu <fitshter@gmail.com>
2022-01-15 20:48:51 +0900

    [add]Extend47

---
+++ Extend47.java
@@ -0,0 +1,12 @@
+package workbook;
+
+public class Extend47 {
+
+	public static void main(String[] args) {
+		
+		// ラッパークラスはObject型と継承関係にある
+		Object obj1 = new int(1);
+		Object obj1 = new Integer(1);
+	}
+
+}
