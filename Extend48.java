6f037baa1208bf4d92a16628c95ab20ba49c55e7
otinu <fitshter@gmail.com>
2022-01-16 17:48:54 +0900

    [add]Extend48

---
+++ Extend48.java
@@ -0,0 +1,19 @@
+package workbook;
+
+public class Extend48 {
+
+	public static void main(String[] args) {
+		
+		// ビット演算
+		
+		int i1 = 5;
+		int i2 = 7;
+		
+		// 101 | 111 ⇒ 111
+		System.out.println(i1 | i2);
+		
+		// 101 & 111 ⇒ 101
+		System.out.println(i1 & i2);
+	}
+
+}
