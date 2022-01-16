a844be2d59520ecef3e3a1ceb587e75f94338869
otinu <fitshter@gmail.com>
2022-01-16 18:22:30 +0900

    [fix]Extend42

--- Extend42.java
+++ Extend42.java
@@ -3,27 +3,44 @@ package workbook;
 public class Extend42 {
 
 	public static void main(String[] args) {
-		
-		// ※floorメソッドでは正の値は、小数点以下切り捨ての「〇.0」
+
+		// ※floor()メソッドでは正の値は、小数点以下切り捨ての「〇.0」
 		//負の値は、負の方向に切り上げての「-〇.0」で表示される
+		System.out.println("floor()の出力================");
 		System.out.println(Math.floor(-0));
-		
+
 		System.out.println(Math.floor(-1));
 		System.out.println(Math.floor(1));
 		System.out.println(Math.floor(-1.11));
 		System.out.println(Math.floor(1.11));
-		
+
 		System.out.println(Math.floor(11.102));
 		System.out.println(Math.floor(11.5));
 		System.out.println(Math.floor(-11.001));
 		System.out.println(Math.floor(-11.5));
-		
+
 		//-----------------------------------------------
-		
-		// ※ceilメソッドでは正の値・負の値ともに正の方向に切り上げられる
+
+		// ※ceil()メソッドでは正の値・負の値ともに正の方向に切り上げられる
+		System.out.println("ceil()の出力================");
 		System.out.println(Math.ceil(-0));
 		System.out.println(Math.ceil(1.11));
 		System.out.println(Math.ceil(-1.11));
+
+		//-----------------------------------------------
+
+		// ※round()メソッドでは四捨五入される
+		System.out.println("round()の出力================");
+		System.out.println(Math.round(-0));
+		System.out.println(Math.round(1.11));
+		System.out.println(Math.round(-1.11));
+		
+		System.out.println(Math.round(1.4));
+		System.out.println(Math.round(1.5));
+		System.out.println(Math.round(-1.4));
+		System.out.println(Math.round(-1.5)); //roundメソッドでは「-〇.5」は正方向への切り上げになる
+		System.out.println(Math.round(-1.6));
+		System.out.println(Math.round(-1.1));
 	}
 
 }
