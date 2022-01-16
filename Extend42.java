c400a10ec5c1f068f0335776b71428452e8a9084
otinu <fitshter@gmail.com>
2022-01-16 17:56:41 +0900

    [fix]Extend42

--- Extend42.java
+++ Extend42.java
@@ -5,7 +5,7 @@ public class Extend42 {
 	public static void main(String[] args) {
 		
 		// ※floorメソッドでは正の値は、小数点以下切り捨ての「〇.0」
-		//負の値は「-〇.0」で表示される
+		//負の値は、負の方向に切り上げての「-〇.0」で表示される
 		System.out.println(Math.floor(-0));
 		
 		System.out.println(Math.floor(-1));
@@ -17,6 +17,13 @@ public class Extend42 {
 		System.out.println(Math.floor(11.5));
 		System.out.println(Math.floor(-11.001));
 		System.out.println(Math.floor(-11.5));
+		
+		//-----------------------------------------------
+		
+		// ※ceilメソッドでは正の値・負の値ともに正の方向に切り上げられる
+		System.out.println(Math.ceil(-0));
+		System.out.println(Math.ceil(1.11));
+		System.out.println(Math.ceil(-1.11));
 	}
 
 }
