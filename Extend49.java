a3775d79cdee6badebe2c93cc17cc83457852539
otinu <fitshter@gmail.com>
2022-01-16 17:49:13 +0900

    [add]Extend49

---
+++ Extend49.java
@@ -0,0 +1,17 @@
+package workbook;
+
+public class Extend49 {
+
+	public static void main(String[] args) {
+		StringBuilder stb = new StringBuilder("tinu");
+		
+		stb = stb.deleteCharAt('i');
+		System.out.println(stb);
+		
+		/*
+		stb.deleteCharAt(2);
+		System.out.println(stb);
+		*/
+	}
+
+}
