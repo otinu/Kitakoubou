4732b9f6374d38ac14af8a7dbc0e0cf6c2d77e9a
otinu <fitshter@gmail.com>
2022-01-16 19:12:30 +0900

    [add]Extend52

---
+++ Extend52.java
@@ -0,0 +1,14 @@
+package workbook;
+
+public class Extend52 {
+
+	public static void main(String[] args) {
+		
+		// long型はdouble型へキャストなしの代入が可能
+		double d = 10L;
+		
+		// intは型変換が必要
+		int i = (int) 10L;
+	}
+
+}
