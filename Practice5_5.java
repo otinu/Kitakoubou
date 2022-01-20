132097bd747a3bac17a0435639a0c5fd5e388f4d
otinu <fitshter@gmail.com>
2022-01-20 21:23:27 +0900

    [fix]Practice5-5

--- Practice5_5.java
+++ Practice5_5.java
@@ -1,9 +1,27 @@
 package workbook;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Practice5_5 {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader( System.in ) );
+		int value = Integer.parseInt( br.readLine() );
+
+		int binary[] = new int[16];
+
+		for( int i = 15 ; i >= 0 ; i--, value /= 2 ) {
+			binary[i] = value % 2;
+		}
+
+
+		for( int i = 0 ; i < 16 ; i++ ) {
+			System.out.print( binary[i] );
+		}
+
 
 	}
 
