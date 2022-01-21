1971d2a7db514367f4ea28e71dd9114d9126835c
otinu <fitshter@gmail.com>
2022-01-21 22:09:59 +0900

    [add]Extend54

---
+++ Extend54.java
@@ -0,0 +1,11 @@
+package workbook;
+
+public class Extend54 {
+
+	public static void main(String[] args) {
+		int i = 10;
+		System.out.println(i == i++); // true
+		System.out.println(i++ == i++); //false
+	}
+
+}
