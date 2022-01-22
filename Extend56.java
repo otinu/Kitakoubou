f6fc70459bb07835b37862e63b487262108e8606
otinu <fitshter@gmail.com>
2022-01-22 11:14:15 +0900

    [fix]Extend56

--- Extend56.java
+++ Extend56.java
@@ -16,6 +16,14 @@ public class Extend56 {
 		// キャストが必要
 		f = i;
 		f = d;
+		
+		//=======================
+		
+		// char型変数に数値リテラルを代入する場合もキャストが必要
+		char t = 97;
+		int i2 = 97;
+		char j = i2;
+		char j2 = (char) i2;
 	}
 
 }
