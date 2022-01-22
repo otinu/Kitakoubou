0be2b8aadc1ed66222ca868572942a81d958d8bc
otinu <fitshter@gmail.com>
2022-01-22 11:24:08 +0900

    [add]Extend57

---
+++ Extend57.java
@@ -0,0 +1,17 @@
+package workbook;
+
+import java.util.ArrayList;
+
+
+// フィールドやメソッドの戻り値にvarを使うことはできない
+public class Extend57 {
+	var v = 1;
+	public static void main(String[] args) {
+		var ex = new ArrayList<Extend57>();
+	}
+	
+	public var test() {
+		return 1;
+	}
+
+}
