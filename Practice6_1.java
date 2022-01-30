2a4142db205e78d09ef4adf11dcf664c9bdca5d0
otinu <fitshter@gmail.com>
2022-01-30 15:17:46 +0900

    [add]Practice6-1

---
+++ Practice6_1.java
@@ -0,0 +1,13 @@
+package workbook;
+
+public class Practice6_1 {
+
+	public static void main(String[] args) {
+		System.out.println(calc(3));
+	}
+	
+	static int calc(int i) {
+		return i * i;
+	}
+
+}
