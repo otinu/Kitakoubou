509a8a97df9d22ecedb77c0abd5d0138e579dbbd
otinu <fitshter@gmail.com>
2022-01-31 09:09:51 +0900

    [add]Practce6-2

---
+++ Practice6_2.java
@@ -0,0 +1,13 @@
+package workbook;
+
+public class Practice6_2 {
+
+	public static void main(String[] args) {
+		System.out.println(avg(2, 8));
+	}
+	
+	static int avg(int i, int j) {
+		return (i + j) / 2;
+	}
+
+}
