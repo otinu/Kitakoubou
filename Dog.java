09d7dbb406c9fae6717b9fccae2fc72694632be5
otinu <fitshter@gmail.com>
2022-02-05 12:28:28 +0900

    [add]Practice7-1

---
+++ Dog.java
@@ -0,0 +1,16 @@
+package workbook;
+
+class Dog
+{
+    private String Name;
+
+    public void SetName( String nm )
+    {
+        Name = nm;
+    }
+
+    public void ShowProfile()
+    {
+        System.out.println( "名前は、" + Name + "です。" );
+    }
+}
---
+++ Practice7_1.java
@@ -0,0 +1,11 @@
+package workbook;
+
+public class Practice7_1 {
+
+	public static void main(String[] args) {
+		Dog toy = new Dog();
+		toy.SetName("プードル");
+		toy.ShowProfile();
+	}
+
+}
