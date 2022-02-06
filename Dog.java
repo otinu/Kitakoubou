55612149c0768569051012ab1f3f4582f075ccae
otinu <fitshter@gmail.com>
2022-02-06 15:57:52 +0900

    [add]Practice7-2

--- Dog.java
+++ Dog.java
@@ -3,14 +3,21 @@ package workbook;
 class Dog
 {
     private String Name;
+    private int Age;
 
     public void SetName( String nm )
     {
         Name = nm;
     }
+    
+    public void SetAge(int age) {
+    	// this.Age = Age;
+    	Age = age;
+    }
 
     public void ShowProfile()
     {
         System.out.println( "名前は、" + Name + "です。" );
+        System.out.println( Age + "歳です。" );
     }
 }
---
+++ Practice7_2.java
@@ -0,0 +1,13 @@
+package workbook;
+
+public class Practice7_2 {
+
+	public static void main(String[] args) {
+		Dog shiba = new Dog();
+		shiba.SetAge(2);
+		shiba.SetName("kotarou");
+		
+		shiba.ShowProfile();
+	}
+
+}
