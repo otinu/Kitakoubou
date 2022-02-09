23528d9327e2c9acb59a2e178ecc1c5ffaade638
otinu <fitshter@gmail.com>
2022-02-10 07:38:38 +0900

    [fix]Practice8-1

--- Cat.java
+++ Cat.java
@@ -1,13 +1,6 @@
 package workbook;
 
 public class Cat extends Animal {
-
-	public static void main(String[] args) {
-		Cat cat = new Cat();
-		cat.SetProfile("tama", 1);
-		cat.ShowProfile();
-		cat.Sleep();
-	}
 	
 	void Sleep() {
 		System.out.println("スースー");
---
+++ Practice8_1.java
@@ -0,0 +1,10 @@
+package workbook;
+
+public class Practice8_1 {
+	public static void main(String[] args) {
+		Cat cat = new Cat();
+		cat.SetProfile("tama", 1);
+		cat.ShowProfile();
+		cat.Sleep();
+	}
+}
